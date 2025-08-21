5e643958d2c6d11cfb8eeb444dca3644db005ddd
aneeshkarthikrh86 <aneeshkarthikrh86@gmail.com>
2025-08-21 13:00:19 +0530

    Keeshparulli

---
+++ JIT99MMK/main_cardgames.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from pages.login_page import Login
+from pages.home_page import HomePage
+from pages.Slot_Providers import SlotProvider
+from pages.Fishing_Provider import FishProvider
+from pages.CardGames_Providers import CardgamesProvider
+from pages.game_page import Game_Click
+
+load_dotenv(override=True)
+
+BASE_URL = os.getenv("BASE_URL")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+if __name__ == "__main__":
+    # Step 1: Login Page actions
+    login_page = Login(BASE_URL)
+    login_page.Start_Browser()
+    login_page.launch_url()
+    print(f"USERNAME from env: {USERNAME!r}")
+    login_page.login(USERNAME, PASSWORD)
+    login_page.Close_Popupbtnscal()
+    
+    # Step 2: Home Page actions
+    home_page = HomePage()
+    home_page.page = login_page.page  # reuse same page
+    home_page.click_Fish()
+    home_page.home_slot()
+    
+    # Step4:- Gameclick close testing
+    CardGames_Providers = CardgamesProvider()
+    CardGames_Providers.page = login_page.page  # reuse same page
+    CardGames_Providers.List_Provisers()
+    
+    # Step 3: Close browser
+    login_page.close_Browser()
---
+++ JIT99MMK/pages/login_page.py
@@ -0,0 +1,23 @@
+import time
+from tests.base_page import BaseClass
+
+class Login(BaseClass):
+    def login(self, username, password):
+        login_btn = self.page.locator("//div[@class='flex items-center gap-2 mr-2']//button[text()='Login']")
+        login_btn.wait_for(state="visible", timeout=12000)
+        login_btn.click()
+        time.sleep(1)
+
+        self.page.wait_for_selector("//div[text() = 'Login with Username']", timeout=12000).click()
+        self.page.wait_for_selector("//input[@placeholder='Username']", timeout=12000).fill(username)
+        time.sleep(1)
+        self.page.wait_for_selector("//input[@placeholder='Password']", timeout=12000).fill(password)
+        self.page.wait_for_selector("//div[@class='relative mt-8']/button[text()='Login']", timeout=12000).click()
+        time.sleep(3)
+
+    def Close_Popupbtnscal(self):
+        try:
+            self.page.wait_for_selector("//div[@style='max-width:600px;']/div/button[@class='mission_daily_close_btn']/img", timeout=3000).click()
+            time.sleep(1)
+        except:
+            pass
